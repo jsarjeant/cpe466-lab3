4f9ddd2b31e0bb0335b6ec1c85626ebd0c601dd3
Joseph Sarjeant <Joseph_Sarjeant@intuit.com>
2015-10-15 14:15:14 -0700

    parameter inputs for main

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from Graph import Graph
+from pagerank import PageRank
+import sys
 
-graph = Graph("karateDir.csv")
+fileName = "karateDir.csv"
+for arg in sys.argv:
+    if ".txt" in arg or ".csv" in arg or ".gml" in arg:
+        fileName = arg
+
+graph = Graph(fileName)
+pr = PageRank(graph.return_graph(), .85)
+
+print "Itterations"
+pr.runPageRankI(100)
+print "\nConverge"
+pr.runPageRankE(.000001)
